e3c078ac0aff65bc3f4f45b11c02564925784896
Meduzz <drgevalia@gmail.com>
2014-06-05 22:00:13 +0200

    Some more DI test utils.

---
+++ src/test/scala/se/chimps/cameltow/framework/dependencyinjection/util/GuiceTestModule.scala
@@ -0,0 +1,12 @@
+package se.chimps.cameltow.framework.dependencyinjection.util
+
+import com.google.inject.AbstractModule
+
+/**
+ * Created by meduzz on 05/06/14.
+ */
+class GuiceTestModule extends AbstractModule {
+  override def configure():Unit = {
+
+  }
+}
---
+++ src/test/scala/se/chimps/cameltow/framework/dependencyinjection/util/SpringTestConfig.scala
@@ -0,0 +1,11 @@
+package se.chimps.cameltow.framework.dependencyinjection.util
+
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Created by meduzz on 05/06/14.
+ */
+@Configuration
+class SpringTestConfig {
+
+}
